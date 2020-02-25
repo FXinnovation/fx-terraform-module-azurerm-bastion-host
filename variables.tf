8d562adbd1ee0ed8b4c6d4f89c3ef7447a2c7ea3
ravi <ravi.periketi@fxinnovation.com>
2020-02-25 12:10:05 -0500

    test:debug

--- variables.tf
+++ variables.tf
@@ -12,6 +12,23 @@ variable "tags" {
   default     = {}
 }
 
+variable "client_id" {
+  default = ""
+}
+
+variable "client_secret" {
+  default = ""
+}
+
+variable "subscription_id" {
+  default = ""
+}
+
+variable "tenant_id" {
+  default = ""
+}
+
+
 ###
 # Public IP
 ###
